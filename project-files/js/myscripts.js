470ec4437d0ff0338f206052805321047b42baf2
Mike <ward.michael51@gmail.com>
2014-04-04 21:57:52 -0400

    Added myscripts.js
    Added additional JS scripts to offer an introduction into user
    interaction.

---
+++ project-files/js/myscripts.js
@@ -0,0 +1,22 @@
+// function myFunction()
+// {
+// var x;
+
+// var person = prompt("Please enter your name"," ");
+
+// if (person!=null)
+//   {
+//   alert(x="Howdy " + person + "! How are you today?");
+  
+//   }
+// }
+
+var button = document.getElementById('action');
+
+button.onclick = function () {
+	alert("Hello " + prompt("What is your name?"));
+}
+
+
+
+
