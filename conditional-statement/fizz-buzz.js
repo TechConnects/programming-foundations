3af0cea0be0c663c0c40c616a9ee958395c92595
TechConnects <techconnects@lfpl.org>
2014-06-26 16:11:23 -0400

    Update fizz-buzz.js

--- conditional-statement/fizz-buzz.js
+++ conditional-statement/fizz-buzz.js
@@ -1,4 +1,6 @@
 //Fizz Buzz
+//Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”.
+
 // for(var i=0; i < 101; i++) {
 //   if (i % 3 == 0) {
 //     console.log("fizz");
