15c7fc64af1dd2e5f68b899ffd55cb973500f048
TechConnects <techconnects@lfpl.org>
2014-06-26 16:09:37 -0400

    Create fizz-buzz.js

---
+++ conditional-statement/fizz-buzz.js
@@ -0,0 +1,12 @@
+//Fizz Buzz
+// for(var i=0; i < 101; i++) {
+//   if (i % 3 == 0) {
+//     console.log("fizz");
+//   }
+//   else if (i % 5 == 0) {
+//     console.log("buzz");
+//   }  
+//   else {
+//     console.log(i);
+//   }
+// }  
